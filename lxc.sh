449b8204e40c5ed4039c0150618620ef9cf66188
Yoonsik Park <park.yoonsik@icloud.com>
2026-03-19 09:22:10 -0400

    Update lxc.sh

--- lxc.sh
+++ lxc.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-# PACKAGES: parted losetup resize2fs blkid kmod-fs-exfat exfat-mkfs gdisk block-mount nano kmod-veth luci-app-lxc lxc-start lxc-stop lxc-destroy lxc-ls
-
+# PACKAGES: luci parted kmod-fs-exfat exfat-mkfs block-mount nano kmod-veth luci-app-lxc lxc-start lxc-stop lxc-destroy lxc-ls kmod-usb-net-qmi-wwan kmod-usb-serial-option luci-proto-mbim picocom luci-proto-qmi kmod-rtw88 kmod-rtw88-8723du hostapd-wolfssl 464xlat
